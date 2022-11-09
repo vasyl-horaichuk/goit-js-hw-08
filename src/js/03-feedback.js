8d85816bdcec8f68d0daaa180089c2c997daebf9
Vasyl Horaichuk <vg.createam@gmail.com>
2022-11-09 17:07:45 +0300

    Виконав 3 завдання

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,33 @@
 import throttle from "lodash.throttle";
 
-
 const formRef = document.querySelector('.feedback-form');
-const emailRef = document.querySelector('input[type="email"]');
-const messageRef = document.querySelector('textarea[name="message"]');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
-console.log(LOCALSTORAGE_KEY );
-
-
 
 formRef.addEventListener('submit', handlerBtnSubmitForm);
-formRef.addEventListener('input', hadlerInputDateForm)
+formRef.addEventListener('input', throttle(hadlerInputDateForm, 500));
+
+
+const object = {}
+
+const hadlerInputDateForm = (e) => {
+    object[e.target.name] = e.target.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(object));
+};
+
+const handlerBtnSubmitForm = (e) => {
+    console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+};
+
+
+(function dataFromLocalStorage() {
+    const data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    const emailRef = document.querySelector('input[type="email"]');
+    const messageRef = document.querySelector('textarea[name="message"]');
+  if (data) {
+    emailRef.value = data.emailRef;
+    messageRef.value = data.messageRef;
+  }
+}());
