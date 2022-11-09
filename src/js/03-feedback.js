e3a5712c083bd75dc98a202850ca9950a28d8cfd
Vasyl Horaichuk <vg.createam@gmail.com>
2022-11-09 17:18:33 +0300

    Виконав 3 завдання

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,33 +1,29 @@
 import throttle from "lodash.throttle";
 
-const formRef = document.querySelector('.feedback-form');
-const LOCALSTORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
 
-formRef.addEventListener('submit', handlerBtnSubmitForm);
-formRef.addEventListener('input', throttle(hadlerInputDateForm, 500));
+const formData = {};
 
+function onFormData(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
 
-const object = {}
-
-const hadlerInputDateForm = (e) => {
-    object[e.target.name] = e.target.value;
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(object));
-};
-
-const handlerBtnSubmitForm = (e) => {
-    console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
-    e.preventDefault();
-    e.currentTarget.reset();
-    localStorage.removeItem(LOCALSTORAGE_KEY);
-};
-
+function onSubmitForm(e) {
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
 
 (function dataFromLocalStorage() {
-    const data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-    const emailRef = document.querySelector('input[type="email"]');
-    const messageRef = document.querySelector('textarea[name="message"]');
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const email = document.querySelector('.feedback-form input');
+  const message = document.querySelector('.feedback-form textarea');
   if (data) {
-    emailRef.value = data.emailRef;
-    messageRef.value = data.messageRef;
+    email.value = data.email;
+    message.value = data.message;
   }
-}());
+})();
